80e4d2eda204fccd319e1bca1bdd66e086f2bf6c
HakunaMatata <kononchuk.elena91@gmail.com>
2023-10-25 17:41:31 +0300

    fix problem with new repo

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -3,7 +3,7 @@ import gulp from 'gulp';
 import ghPages from 'gulp-gh-pages';
 
 function deploy() {
-   return gulp.src('../../dist/**/*')
+   return gulp.src('./dist/**/*')
       .pipe(ghPages());
 }
 
